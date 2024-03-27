f843b55cf62beb91668a80687d6e2b2b85c684e0
Nguyen Son Linh <linhns59@gmail.com>
2024-03-28 00:54:58 +0800

    Use mini pairs and surround plugin

--- nvim/lua/linhns/plugins/pairs.lua
+++ nvim/lua/linhns/plugins/pairs.lua
@@ -1,11 +1,11 @@
 local M = {
-    "echasnovski/mini.surround",
+    "echasnovski/mini.pairs",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
 }
 
 M.config = function()
-    require("mini.surround").setup({
+    require("mini.pairs").setup({
         -- Configuration here, or leave empty to use defaults
     })
 end
