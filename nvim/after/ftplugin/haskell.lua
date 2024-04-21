96ce903368ef43d0cfb659f1636146437299799c
Nguyen Son Linh <linhns59@gmail.com>
2024-04-22 01:02:50 +0800

    Add more keymaps

--- nvim/after/ftplugin/haskell.lua
+++ nvim/after/ftplugin/haskell.lua
@@ -5,22 +5,34 @@ local opts = { noremap = true, silent = true, buffer = bufnr }
 local keymap = vim.keymap
 -- haskell-language-server relies heavily on codeLenses,
 -- so auto-refresh (see advanced configuration) is enabled by default
-keymap.set("n", "<space>cl", vim.lsp.codelens.run, opts)
+opts.desc = "Run code lenses"
+keymap.set("n", "<leader>cl", vim.lsp.codelens.run, opts)
+
 -- Hoogle search for the type signature of the definition under the cursor
-keymap.set("n", "<space>hs", ht.hoogle.hoogle_signature, opts)
+opts.desc = "Hoogle search for the type signature of the definition under the cursor"
+keymap.set("n", "<localleader>hs", ht.hoogle.hoogle_signature, opts)
+
 -- Evaluate all code snippets
-keymap.set("n", "<space>ea", ht.lsp.buf_eval_all, opts)
+opts.desc = "Evaluate all code snippets"
+keymap.set("n", "<localleader>ea", ht.lsp.buf_eval_all, opts)
+
 -- Toggle a GHCi repl for the current package
-keymap.set("n", "<leader>rr", ht.repl.toggle, opts)
+opts.desc = "Toggle a GHCi repl for the current package"
+keymap.set("n", "<localleader>rr", ht.repl.toggle, opts)
+
+-- Restart hls
+opts.desc = "Restart hls"
+keymap.set("n", "<localleader>rs", ht.lsp.restart, opts)
+
 -- Toggle a GHCi repl for the current buffer
-keymap.set("n", "<leader>rf", function()
+opts.desc = "Toggle a GHCi repl for the current buffer"
+keymap.set("n", "<localleader>rf", function()
     ht.repl.toggle(vim.api.nvim_buf_get_name(0))
 end, opts)
-keymap.set("n", "<leader>rq", ht.repl.quit, opts)
 
-keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-keymap.set("n", "K", vim.lsp.buf.hover, opts)
+-- Quit the GHCi repl
+opts.desc = "Quit the GHCi repl"
+keymap.set("n", "<localleader>rq", ht.repl.quit, opts)
 
 local opt_local = vim.opt_local
 opt_local.softtabstop = 2
