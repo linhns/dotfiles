5bd5a61dd914e69f88d27773fb67c1acd88fe4a6
Nguyen Son Linh <linhns59@gmail.com>
2024-06-01 01:20:20 +0800

    Change to kanagawa

--- nvim/lua/linhns/plugins/lualine.lua
+++ nvim/lua/linhns/plugins/lualine.lua
@@ -1,163 +1,47 @@
---- @param trunc_width number trunctates component when screen width is less then trunc_width
---- @param trunc_len number truncates component to trunc_len number of chars
---- @param hide_width number | nil hides component when window width is smaller then hide_width
---- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
---- return function that can format the component accordingly
-local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
-    return function(str)
-        local win_width = vim.fn.winwidth(0)
-        if hide_width and win_width < hide_width then
-            return ""
-        elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
-            return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
-        end
-        return str
-    end
-end
-
--- Eviline config for lualine
--- Author: shadmansaleh
--- Credit: glepnir
-
--- Color table for highlights
-local colors = {
-    bg = "#2d333b",
-    fg = "#bbc2cf",
-    yellow = "#ECBE7B",
-    cyan = "#008080",
-    darkblue = "#081633",
-    green = "#98be65",
-    orange = "#FF8800",
-    violet = "#a9a1e1",
-    magenta = "#c678dd",
-    blue = "#51afef",
-    red = "#ec5f67",
-}
-
-local conditions = {
-    buffer_not_empty = function()
-        return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
-    end,
-    hide_in_width = function()
-        return vim.fn.winwidth(0) > 80
-    end,
-    check_git_workspace = function()
-        local filepath = vim.fn.expand("%:p:h")
-        local gitdir = vim.fn.finddir(".git", filepath .. ";")
-        return gitdir and #gitdir > 0 and #gitdir < #filepath
-    end,
-}
-
--- Config
-local config = {
-    options = {
-        -- Disable sections and component separators
-        component_separators = "",
-        section_separators = "",
-        theme = {
-            -- We are going to use lualine_c an lualine_x as left and
-            -- right section. Both are highlighted by c theme .  So we
-            -- are just setting default looks o statusline
-            normal = { c = { fg = colors.fg, bg = colors.bg } },
-            inactive = { c = { fg = colors.fg, bg = colors.bg } },
-        },
-    },
-    sections = {
-        -- these are to remove the defaults
-        lualine_a = {},
-        lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
-        -- These will be filled later
-        lualine_c = {},
-        lualine_x = {},
-    },
-    inactive_sections = {
-        -- these are to remove the defaults
-        lualine_a = {},
-        lualine_b = {},
-        lualine_y = {},
-        lualine_z = {},
-        lualine_c = {},
-        lualine_x = {},
+local M = {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "AndreM222/copilot-lualine",
     },
 }
 
--- Inserts a component in lualine_c at left section
-local function ins_left(component)
-    table.insert(config.sections.lualine_c, component)
-end
-
--- Inserts a component in lualine_x at right section
-local function ins_right(component)
-    table.insert(config.sections.lualine_x, component)
-end
-
-ins_left({
-    -- mode component
-    "mode",
-    color = function()
-        -- auto change color according to neovims mode
-        local mode_color = {
-            n = colors.blue,
-            i = colors.green,
-            v = colors.magenta,
-            ["\022"] = colors.magenta,
-            V = colors.magenta,
-            c = colors.violet,
-            no = colors.blue,
-            s = colors.orange,
-            S = colors.orange,
-            ["\019"] = colors.orange,
-            ic = colors.green,
-            R = colors.red,
-            Rv = colors.red,
-            cv = colors.red,
-            ce = colors.red,
-            r = colors.cyan,
-            rm = colors.cyan,
-            ["r?"] = colors.cyan,
-            ["!"] = colors.red,
-            t = colors.darkblue,
-        }
-        local mode = vim.fn.mode()
-        return {
-            bg = mode_color[mode],
-            fg = mode == "t" and colors.fg or colors.bg,
-        }
-    end,
-    padding = 1,
-})
-
-ins_left({
-    "branch",
-    icon = "",
-    color = { fg = colors.violet, gui = "bold" },
-    on_click = function()
-        if conditions.check_git_workspace() then
-            -- local telescope = require("telescope")
-            local builtin = require("telescope.builtin")
-            builtin.git_branches()
-        end
-    end,
-})
-
-ins_left({
-    "diff",
-    -- Is it me or the symbol for modified us really weird
-    -- symbols = { added = " ", modified = "󰝤 ", removed = " " },
-    diff_color = {
-        added = { fg = colors.green },
-        modified = { fg = colors.orange },
-        removed = { fg = colors.red },
+local diagnostics = {
+    "diagnostics",
+    symbols = {
+        error = " ",
+        warn = " ",
+        info = " ",
     },
-    cond = conditions.hide_in_width,
-})
+}
 
-ins_left({
-    -- Lsp server name .
+local fileformat = {
+    "fileformat",
+    icons_enabled = true,
+    symbols = {
+        unix = "LF",
+        dos = "CRLF",
+        windows = "CR",
+    },
+}
+
+local filetype = {
+    "filetype",
+    icon_only = true,
+    icon = {
+        align = "right",
+    },
+    padding = { left = 1, right = 0 },
+}
+
+local filename = {
+    "filename",
+    padding = { left = 0, right = 1 },
+}
+
+local lsp = {
     function()
-        local msg = "No LSP"
+        local msg = ""
         local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
         local clients = vim.lsp.get_active_clients()
         if next(clients) == nil then
@@ -171,72 +55,23 @@ ins_left({
         end
         return msg
     end,
-    icon = "::",
-    color = { fg = colors.blue },
-})
-
-ins_left({
-    "diagnostics",
-    sources = { "nvim_diagnostic" },
-    -- symbols = { error = " ", warn = " ", info = " " },
-    diagnostics_color = {
-        color_error = { fg = colors.red },
-        color_warn = { fg = colors.yellow },
-        color_info = { fg = colors.cyan },
-    },
-    on_click = function()
-        vim.cmd("Trouble")
-    end,
-})
-
--- Add components to right sections
-ins_right({ "progress", color = { fg = colors.fg, gui = "bold" } })
-
-ins_right({ "location" })
-
-ins_right({
-    -- filesize component
-    "filesize",
-    cond = conditions.buffer_not_empty,
-    color = { fg = colors.magenta },
-})
-
-ins_right({
-    "o:encoding", -- option component same as &encoding in viml
-    fmt = string.upper, -- I'm not sure why it's upper case either ;)
-    cond = conditions.hide_in_width,
-    color = { fg = colors.green, gui = "bold" },
-})
-
-ins_right({
-    "fileformat",
-    -- fmt = string.upper,
-    -- icons_enabled = true, -- I think icons are cool but Eviline doesn't have them. sigh
-    color = { fg = colors.green, gui = "bold" },
-    symbols = { unix = "LF", dos = "CRLF", mac = "LF" },
-})
-
-ins_right({
-    "filetype",
-})
-
-ins_right({
-    "copilot",
-    cond = conditions.hide_in_width,
-    -- color = { fg = colors.green, gui = "bold" },
-})
-
--- Now don't forget to initialize lualine
-local M = {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-        "AndreM222/copilot-lualine",
-    },
-    config = function()
-        local lualine = require("lualine")
-        lualine.setup(config)
-    end,
+    icon = " ",
 }
+
+M.config = function()
+    local lualine = require("lualine")
+    lualine.setup({
+        options = {
+            section_separators = { left = "", right = "" },
+            component_separators = { left = "", right = "" },
+        },
+        sections = {
+            lualine_b = { "branch", "diff" },
+            lualine_c = { filetype, filename, diagnostics },
+            lualine_x = { lsp, "copilot", "encoding" },
+            lualine_y = { fileformat, "progress" },
+        },
+    })
+end
 
 return M
