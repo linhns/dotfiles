f19dae9a1ff36966f4a5e708bb62f3f816e743b0
Nguyen Son Linh <linhns59@gmail.com>
2024-08-14 00:48:17 +0800

    Rename in workspace

--- nvim/lua/plugins/mini/files.lua
+++ nvim/lua/plugins/mini/files.lua
@@ -29,3 +29,44 @@ vim.api.nvim_create_autocmd("User", {
         vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
     end,
 })
+
+---@param from string
+---@param to string
+local lsp_on_rename = function(from, to)
+    local changes = {
+        files = {
+            {
+                oldUri = vim.uri_from_fname(from),
+                newUri = vim.uri_from_fname(to),
+            },
+        },
+    }
+
+    local clients = vim.lsp.get_clients()
+    for _, client in ipairs(clients) do
+        if client.supports_method("workspace/willRenameFiles") then
+            local resp = client.request_sync(
+                "workspace/willRenameFiles",
+                changes,
+                1000,
+                0
+            )
+            if resp and resp.result ~= nil then
+                vim.lsp.util.apply_workspace_edit(
+                    resp.result,
+                    client.offset_encoding
+                )
+            end
+        end
+        if client.supports_method("workspace/didRenameFiles") then
+            client.notify("workspace/didRenameFiles", changes)
+        end
+    end
+end
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "MiniFilesActionRename",
+    callback = function(event)
+        lsp_on_rename(event.data.from, event.data.to)
+    end,
+})
