5ea119319c3ebefbdac5ab28946b2f8ddfc59545
Nguyen Son Linh <linhns59@gmail.com>
2024-08-15 02:18:17 +0800

    Enable snippet completion

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,63 +1,76 @@
 local lspconfig = require("lspconfig")
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig["gopls"].setup({
-	settings = {
-		gopls = {
-			analyses = {
-				unusedvariable = true,
-			},
-			gofumpt = true,
-		},
-	},
+    capabilities = capabilities,
+    settings = {
+        gopls = {
+            codelenses = {
+                test = true,
+            },
+            analyses = {
+                unusedvariable = true,
+            },
+            gofumpt = true,
+            usePlaceholders = true,
+        },
+    },
 })
 
 lspconfig["clangd"].setup({
-	capabilities = vim.tbl_deep_extend("error", capabilities, {
-		offsetEncoding = { "utf-16" },
-	}),
-	cmd = {
-		"clangd",
-		"--clang-tidy",
-		"--header-insertion=iwyu",
-		"--completion-style=detailed",
-		"--function-arg-placeholders",
-		"--fallback-style=none",
-	},
+    capabilities = vim.tbl_deep_extend("error", capabilities, {
+        offsetEncoding = { "utf-16" },
+    }),
+    cmd = {
+        "clangd",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=none",
+    },
 })
 
 lspconfig["lua_ls"].setup({
-	on_init = function(client)
-		local path = client.workspace_folders[1].name
-		if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-			return
-		end
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if
+            vim.loop.fs_stat(path .. "/.luarc.json")
+            or vim.loop.fs_stat(path .. "/.luarc.jsonc")
+        then
+            return
+        end
 
-		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-			runtime = {
-				-- Tell the language server which version of Lua you're using
-				-- (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			-- Make the server aware of Neovim runtime files
-			workspace = {
-				checkThirdParty = false,
-				library = {
-					vim.env.VIMRUNTIME,
-					-- Depending on the usage, you might want to add additional paths here.
-					"${3rd}/luv/library",
-					-- "${3rd}/busted/library",
-				},
-			},
-		})
-		client.notify(vim.lsp.protocol.Methods.workspace_didChangeConfiguration, { settings = client.config.settings })
-	end,
-	settings = {
-		Lua = {
-			completion = {
-				callSnippet = "Replace",
-			},
-		},
-	},
+        client.config.settings.Lua =
+            vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using
+                    -- (most likely LuaJIT in the case of Neovim)
+                    version = "LuaJIT",
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                    checkThirdParty = false,
+                    library = {
+                        vim.env.VIMRUNTIME,
+                        -- Depending on the usage, you might want to add additional paths here.
+                        "${3rd}/luv/library",
+                        -- "${3rd}/busted/library",
+                    },
+                },
+            })
+        client.notify(
+            vim.lsp.protocol.Methods.workspace_didChangeConfiguration,
+            { settings = client.config.settings }
+        )
+    end,
+    settings = {
+        Lua = {
+            completion = {
+                callSnippet = "Replace",
+            },
+        },
+    },
 })
