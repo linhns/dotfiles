415e12f65fe5e3ab5f4592365b9a319a7a8474f3
Nguyen Son Linh <linhns59@gmail.com>
2024-09-21 21:15:07 +0800

    Center Pick UI

--- nvim/lua/plugins/mini/pick.lua
+++ nvim/lua/plugins/mini/pick.lua
@@ -1,10 +1,20 @@
+local win_config = function()
+    local height = math.floor(0.618 * vim.o.lines)
+    local width = math.floor(0.618 * vim.o.columns)
+    return {
+        anchor = "NW",
+        height = height,
+        width = width,
+        row = math.floor(0.5 * (vim.o.lines - height)),
+        col = math.floor(0.5 * (vim.o.columns - width)),
+    }
+end
+
 local minipick = require("mini.pick")
 require("mini.pick").setup({
-	window = {
-		config = {
-			border = "single",
-		},
-	},
+    window = {
+        config = win_config,
+    },
 })
 
 vim.ui.select = minipick.ui_select
