52c75232ad3507752c5dad7cf95fdca7d84c1f02
Nguyen Son Linh <linhns59@gmail.com>
2025-05-21 23:29:39 +0700

    fix: remove wrong condition

--- .config/nvim/lua/plugins/mason-tool-installer.lua
+++ .config/nvim/lua/plugins/mason-tool-installer.lua
@@ -1,28 +1,16 @@
-local check_go_exists = function()
-    return not os.execute("go version")
-end
-
-local check_cmake_exists = function()
-    return not os.execute("cmake --version")
-end
-
-local check_python_exists = function()
-    return not os.execute("python3 --version")
-end
-
 require("mason-tool-installer").setup({
     ensure_installed = {
-        { "gopls", condition = check_go_exists },
-        { "delve", condition = check_go_exists },
-        { "golangci-lint", condition = check_go_exists },
-        { "gomodifytags", condition = check_go_exists },
-        { "goimports", condition = check_go_exists },
-        { "impl", condition = check_go_exists },
-        { "gotests", condition = check_go_exists },
-        { "cmake-language-server", condition = check_cmake_exists },
-        { "cmakelint", condition = check_cmake_exists },
-        { "python-lsp-server", condition = check_python_exists },
-        { "ruff", condition = check_python_exists },
+        "gopls",
+        "delve",
+        "golangci-lint",
+        "gomodifytags",
+        "goimports",
+        "impl",
+        "gotests",
+        "cmake-language-server",
+        "cmakelint",
+        "python-lsp-server",
+        "ruff",
         "markdownlint",
         "stylua",
         "lua-language-server",
@@ -32,4 +20,5 @@ require("mason-tool-installer").setup({
         "shellcheck",
         "bash-language-server",
     },
+    run_on_start = true,
 })
