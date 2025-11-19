92a8fceddec317452d493f6c44a74bd140291655
Nguyen Son Linh <linhns59@gmail.com>
2025-11-19 23:22:33 +0700

    feat: remove Windows config

--- profile.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module -Name Terminal-Icons
-Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource History
-$ENV:STARSHIP_CONFIG = "C:\Users\linhn\dotfiles\starship.toml"
-Invoke-Expression (&starship init powershell)
